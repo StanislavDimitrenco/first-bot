5d60ab8161ea12c2e64c21f031ad9215d9979f06
stanislavdimitrenco <stanislav.dimitrenco@gmail.com>
2020-11-28 22:10:32 +0200

    Hidden bot's key

--- constant.go
+++ constant.go
@@ -1,6 +1,5 @@
 package main
 
-const TelegramKey = "1427431817:AAGs2h-Q1o2fF4msSli1IAfX7_FI-jH2tTE"
 const TelegramBaseUrl = "https://api.telegram.org/bot"
 const MethodGetMe = "getMe"
 const MethodGetUpdates = "getUpdates"
---
+++ key.go
@@ -0,0 +1,3 @@
+package main
+
+const TelegramKey = "1427431817:AAGs2h-Q1o2fF4msSli1IAfX7_FI-jH2tTE"
